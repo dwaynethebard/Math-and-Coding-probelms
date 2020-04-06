4607b9fd3484511b97d3ff42235bb44f2f476aab
Darren Ytsma <darren.ytsma@gmail.com>
2020-04-05 20:46:18 -0700

    Create main.py

---
+++ Project Euler/Problem4/main.py
@@ -0,0 +1,12 @@
+n = 0
+
+for a in range(999, 100, -1):
+    for b in range(a, 100, -1):
+        x = a * b
+        if x > n:
+            s = str(a * b)
+            if s == s[::-1]:
+                n = a * b
+                A=a
+                B=b
+print(n,A,B)
