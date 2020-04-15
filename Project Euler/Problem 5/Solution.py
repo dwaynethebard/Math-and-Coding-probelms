e9c7c3bbde3ddae73821be9f97ed5fc3b62b0477
Darren Ytsma <darren.ytsma@gmail.com>
2020-04-15 11:34:47 -0700

    Update Solution.py

--- Project Euler/Problem 5/Solution.py
+++ Project Euler/Problem 5/Solution.py
@@ -1,6 +1,6 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? 
 import math
-def lcm(x,y): return x * y / math.gcd(x,y)
+def lcm(x,y): return int(x * y / math.gcd(x,y))
 
 n = 1
 for i in range(1, 20):
