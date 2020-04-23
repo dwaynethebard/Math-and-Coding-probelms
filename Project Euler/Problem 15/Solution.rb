3080c2a25bfee875c335dd8b83d4687ac1df97ca
Darren Ytsma <darren.ytsma@gmail.com>
2020-04-23 10:24:07 -0700

    Update Solution.rb

--- Project Euler/Problem 15/Solution.rb
+++ Project Euler/Problem 15/Solution.rb
@@ -7,13 +7,12 @@ start = Time.now
 
 #Take the 2x2 grid, to reach the end we have 4 steps to take. R,R,D,D. now this is just a simple permutation
 #4!/2!2!. For a MxN grid the formula would be (M+N)!/M!N!
+# for a NxN grid this can be reduced to (2*N)*(2*N-1)...(N+1)/N!
 
-# Ruby dosen't have a Factoral function
-# So we use the gamma function Math.gamma returns (n-1)! so we must call n+1
-# or just simply n.downto(1).inject(:*) would work
-def lattice_path_permutation!(n,m)
-    (Math.gamma(n+m+1)/(Math.gamma(m+1)*Math.gamma(n+1))).ceil 
+
+def lattice_path_permutation!(n)
+    (2*n).downto(n+1).inject(:*)/(n).downto(1).inject(:*) 
 end
 stop = Time.now
-print lattice_path_permutation!(20,20),"\n"
+print lattice_path_permutation!(20),"\n"
 print "The program took ", stop-start," ms to complete"
